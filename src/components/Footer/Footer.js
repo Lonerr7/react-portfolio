f70b9cff883656a2acf4df3052e07dabce2e6515
Lonerr7 <krabik2342@gmail.com>
2022-03-20 12:56:50 +0300

    6. Created Footer.js and pinned it to the bottom

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,7 @@ const Footer = () => {
         <div className={s.footer__inner}>
           <div className={s.footer__links}>
             <a
+              className={s.footer__link}
               href="https://github.com/Lonerr7"
               rel="noreferrer"
               target="_blank"
@@ -15,10 +16,15 @@ const Footer = () => {
               GitHub
             </a>
           </div>
-          <p>
-            © 2021.
-            <a href="https://lmpixels.com/wp/leven-wp/full-width/">
-              Design was taken from
+          <p className={s.footer__text}>
+            © 2022. 
+            <a
+              className={s.footer__textLink}
+              href="https://lmpixels.com/wp/leven-wp/full-width/"
+              target="_blank"
+              rel="noreferrer"
+            >
+                Design was taken from here
             </a>
           </p>
         </div>
