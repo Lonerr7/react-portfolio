2c15af1abdbb904af6ffa293ca22ecbef0e4e3cd
Lonerr7 <krabik2342@gmail.com>
2022-03-29 19:03:56 +0300

    11.5. Made active class toggling in PortfolioControls.js

--- src/components/Main/Portfolio/PortfolioItems/PortfolioControls/PortfolioControls.js
+++ src/components/Main/Portfolio/PortfolioItems/PortfolioControls/PortfolioControls.js
@@ -1,26 +1,48 @@
+import { useState } from 'react';
 import s from './PortfolioControls.module.scss';
 
 const PortfolioControls = ({ filterPortfolioItemsHandler }) => {
+  const [active, setActive] = useState(1);
+
   return (
     <div className={s.portfolioControls}>
       <ul className={s.portfolioControls__list}>
         <li
-          className={`${s.portfolioControls__item} ${s.active}`}
+          className={
+            active === 1
+              ? `${s.portfolioControls__item} ${s.active}`
+              : s.portfolioControls__item
+          }
           onClick={() => {
             filterPortfolioItemsHandler('');
+            setActive(1);
           }}
         >
           All
         </li>
         <li
-          className={s.portfolioControls__item}
-          onClick={() => filterPortfolioItemsHandler('react')}
+          className={
+            active === 2
+              ? `${s.portfolioControls__item} ${s.active}`
+              : s.portfolioControls__item
+          }
+          onClick={() => {
+            filterPortfolioItemsHandler('react');
+            setActive(2);
+          }}
         >
           ReactJs
         </li>
         <li
-          className={s.portfolioControls__item}
-          onClick={() => filterPortfolioItemsHandler('landing')}
+          className={
+            active === 3
+              ? `${s.portfolioControls__item} ${s.active}`
+              : s.portfolioControls__item
+          }
+          onClick={() => {
+            filterPortfolioItemsHandler('landing');
+            setActive(3);
+          }}
         >
           HTML/CSS
         </li>
